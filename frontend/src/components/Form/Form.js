4aef672b6738508aa1cdee222dc93457a6a900e0
mfbernasol <mfbernasol@gmail.com>
2022-09-13 15:09:28 -0500

    setup redux

---
+++ frontend/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import { useTheme } from '@material-ui/styles';
+import React from 'react';
+
+import useStyles from './styles';
+const Form = () => {
+    const classes = useStyles();
+    return (
+        <h1>FORM</h1>
+    )
+}
+
+
+export default Form;
